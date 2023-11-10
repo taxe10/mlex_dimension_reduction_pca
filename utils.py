735c2374ac95b7d4b4e895e6dc6f810b2c2a7ce0
Runbo Jiang <rjiang2@lbl.gov>
2023-11-10 09:51:38 -0800

    minor update

--- utils.py
+++ utils.py
@@ -12,15 +12,11 @@ def load_images_from_directory(directory_path):
         if filename.endswith(".png"):
             file_path = os.path.join(directory_path, filename)
             try:
-                # Open the image using Pillow
                 img = Image.open(file_path)
-                # Convert the image to a numpy array
                 img_array = np.array(img)
-                # Append the image array to the list
                 image_data.append(img_array)
             except Exception as e:
                 print(f"Error processing {file_path}: {e}")
     
-    # Convert the list of image arrays to a numpy array
     image_data = np.array(image_data)
     return image_data
