0396d44e6696e2b5710a3bb5fa6d1fec69fcf6f0
taxe10 <tanchavez@lbl.gov>
2024-06-04 17:46:56 -0700

    Fix import

--- src/_test/test_pca.py
+++ src/_test/test_pca.py
@@ -1,4 +1,3 @@
-import pytest
 import numpy as np
 
 from mlex_dimension_reduction_pca.pca_run import computePCA
@@ -9,18 +8,23 @@ def test_1d_input():
     result = computePCA(data, n_components=2)
     assert result.shape == (100, 2), "Output shape should be (N, 2)"
 
+
 def test_2d_input():
     data = np.random.rand(100, 10, 10)
     result = computePCA(data, n_components=2)
     assert result.shape == (100, 2), "Output shape should be (N, 2)"
+
 
 def test_n_components_3():
     data = np.random.rand(100, 50)
     result = computePCA(data, n_components=3)
     assert result.shape == (100, 3), "Output shape should be (N, 3) when n_components=3"
 
+
 def test_reproducibility():
     data = np.random.rand(100, 50)
     result1 = computePCA(data, n_components=2)
     result2 = computePCA(data, n_components=2)
-    np.testing.assert_array_almost_equal(result1, result2, err_msg="Results should be the same for the same input data")
+    np.testing.assert_array_almost_equal(
+        result1, result2, err_msg="Results should be the same for the same input data"
+    )
