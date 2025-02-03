06bf419862d7f7835413e589d1a9427689fd7bcb
Tanny Chavez <tanchavez@lbl.gov>
2025-02-02 16:05:26 -0800

    Check if tiled_results_uri is a container or array

--- src/utils/tiled_utils.py
+++ src/utils/tiled_utils.py
@@ -1,4 +1,7 @@
+import numpy as np
 from tiled.client import from_uri
+from tiled.client.array import ArrayClient
+from tiled.client.container import Container
 from tiled.structures.data_source import Asset, DataSource
 from tiled.structures.table import TableStructure
 
@@ -12,14 +15,23 @@ class TiledDataset:
         self.write_client = from_uri(write_tiled_uri, api_key=write_tiled_key)
 
     def load_data_from_tiled(self, uri):
-        return self.read_client[uri][:]
+        data_client = self.read_client[uri]
+        if isinstance(data_client, Container):
+            data = []
+            sub_uris = list(data_client)
+            for sub_uri in sub_uris:
+                if isinstance(data_client[sub_uri], ArrayClient):
+                    data.append(self.read_client[uri][sub_uri][:])
+            return np.stack(data, axis=0)
+        elif isinstance(data_client, ArrayClient):
+            return self.read_client[uri][:]
+        else:
+            raise ValueError(f"Data structure not supported: {type(data_client)}")
 
     def write_results(
         self, latent_vectors, io_parameters, latent_vectors_path, metadata=None
     ):
-        # Prepare Tiled parent node
         uid_save = io_parameters.uid_save
-        parent_node = self.write_client["latent_vectors"]
 
         # Save latent vectors to Tiled
         structure = TableStructure.from_pandas(latent_vectors)
@@ -27,15 +39,15 @@ class TiledDataset:
         # Remove API keys from metadata
         if metadata:
             metadata["io_parameters"].pop("data_tiled_api_key", None)
-            metadata["io_parameters"].pop("result_tiled_api_key", None)
+            metadata["io_parameters"].pop("results_tiled_api_key", None)
 
-        frame = parent_node.new(
+        frame = self.write_client.new(
             structure_family="table",
             data_sources=[
                 DataSource(
                     structure_family="table",
                     structure=structure,
-                    mimetype="application/octet-stream",
+                    mimetype="application/x-parquet",
                     assets=[
                         Asset(
                             data_uri=f"file://{latent_vectors_path}",
